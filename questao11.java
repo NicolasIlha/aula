6cc1037a209fc4164a13654e48eeb1c8c71723d0
Nicolas Lobur <135863723+NicolasIlha@users.noreply.github.com>
2023-09-13 12:44:43 -0300

    Add files via upload

---
+++ questao11.java
@@ -0,0 +1,11 @@
+public class questao11 {
+    public static void main(String[] args) {
+        String texto = "string ";
+        int tamanho = texto.length();
+        int metade = tamanho / 2;
+        
+        String primeiraMetade = texto.substring(0, metade);
+        
+        System.out.println("Primeira metade: " + primeiraMetade);
+    }
+}
