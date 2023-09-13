aeb409b8d1bdf1eee51ee884e40767aea6aeb1d4
Nicolas Lobur <135863723+NicolasIlha@users.noreply.github.com>
2023-09-13 09:24:03 -0300

    Update LerNome.java

--- LerNome.java
+++ LerNome.java
@@ -4,9 +4,9 @@ public class LerNome {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Escreva seu nome: ");
+        System.out.print( "Escreva seu nome: " );
         String nome = scan.nextLine();
         System.out.println();
-        System.out.println("Seu nome é "+nome);
+        System.out.println("Seu nome é " +  nome);
     }
 }
